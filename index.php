2127d9e55b9c3b652a5bb39c1850cc43d8e7d395
Hain-Lee Hsueh <leehsueh7@gmail.com>
2011-09-08 20:46:06 -0700

    changed deployment directory to 1.1.8 framework

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $deploy = false;
 
 // latest yii framework
 if ($deploy) {
-	$yii='/home/leehsueh/yii_framework/yii.php';
+	$yii='/home/leehsueh/yii_framework-1.1.8/yii.php';
 	$config=dirname(__FILE__).'/protected/config/main.php';
 	$YII_DEBUG = false;
 } else {
